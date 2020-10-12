96798a72e295e2316d65673c8a7067604253762d
aaronwb <zmtzwb@gmail.com>
2020-10-12 15:54:52 +0800

    add view

---
+++ app/src/main/java/vip/fastgo/tracer/views/LockActivity.java
@@ -0,0 +1,16 @@
+package vip.fastgo.tracer.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vip.fastgo.tracer.R;
+
+public class LockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock);
+    }
+}
